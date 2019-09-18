00308a63e51bdff699106efa5b8ca816bbd7e12e
race-briggs <race.briggs@gmail.com>
2019-09-18 12:59:41 -0500

    added firebase db and form

---
+++ assets/javascript/timesheet.js
@@ -0,0 +1,16 @@
+  // Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCNIiMkn9HcaR2z0WKY8tpokRbgDbC8WJU",
+  authDomain: "superdooperpartypooper-3f721.firebaseapp.com",
+  databaseURL: "https://superdooperpartypooper-3f721.firebaseio.com",
+  projectId: "superdooperpartypooper-3f721",
+  storageBucket: "",
+  messagingSenderId: "835144134075",
+  appId: "1:835144134075:web:4c5de2eba29b5a0859289e"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
